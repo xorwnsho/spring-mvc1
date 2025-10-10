7c414ff5455053c7799359a86f30dc3f9d4377e8
OhJunTaekk <mt3026super@naver.com>
2025-10-10 15:09:23 +0900

    ~2025.10.10

--- src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -1,5 +1,7 @@
 package hello.servlet.web.frontcontroller.v3;
 
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
@@ -24,6 +26,31 @@ public class FrontControllerServletV3 extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV3 controller = controllerMap.get(requestURI);
 
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+        view.render(mv.getModel(), request, response);
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request){
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator().
+                forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+
+
+    private MyView viewResolver(String viewName){
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
